aa3c22eab4878a7ed239d892c68d598cec78bb89
Rohan Kurian <rohankurian10@hotmail.com>
2021-12-15 02:31:49 +0530

    added login and reg link to home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,12 @@
 import "./Home.css";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div className="Home">
       <h1>Home Page</h1>
+      <Link to="/login">Login Page</Link>
+      <Link to="/registration">Registration Page</Link>
     </div>
   );
 };
