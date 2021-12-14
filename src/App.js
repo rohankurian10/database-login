766466fda11187f0469d59ef64923be8c567ecf8
Rohan Kurian <rohankurian10@hotmail.com>
2021-12-14 15:52:01 +0530

    Added all necessary components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
+import "./App.css";
 
 const App = () => {
-  return <div className="App">
-    
-  </div>;
+  return <div className="App"></div>;
 };
 
 export default App;
