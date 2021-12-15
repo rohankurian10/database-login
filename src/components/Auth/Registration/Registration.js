766fd50d61b0f215d7431c5bcfc144458fd0e8ab
Rohan Kurian <rohankurian10@hotmail.com>
2021-12-15 12:41:39 +0530

    minor css changes and addefdform

--- src/components/Auth/Registration/Registration.js
+++ src/components/Auth/Registration/Registration.js
@@ -1,24 +1,39 @@
 import "./Registration.css";
 import { Link } from "react-router-dom";
+import { useRef } from "react";
 
 const Registration = () => {
+  let name = useRef();
+  let email = useRef();
+  let password = useRef();
+  let confirmPassword = useRef();
+
+  let nameValidator = () => {
+    let name;
+  };
   return (
     <div className="Registration">
-      <div className="Header-Container">
-        <h1>Registration</h1>
-      </div>
-      <div className="Input-Container">
-        <input type="text" placeholder="name"></input>
-        <input type="email" placeholder="email"></input>
-        <input type="password" placeholder="password"></input>
-        <input type="password" placeholder="confirm password"></input>
-      </div>
-      <div className="Button-Container">
-        <button>REGISTER</button>
-        <div>
-          <Link to="/login">Already Registered? Login</Link>
+      <form name="Registration" className="Form-Container">
+        <div className="Header-Container">
+          <h1>Registration</h1>
         </div>
-      </div>
+        <div className="Input-Container">
+          <input ref={name} type="text" placeholder="name"></input>
+          <input ref={email} type="email" placeholder="email"></input>
+          <input ref={password} type="password" placeholder="password"></input>
+          <input
+            ref={confirmPassword}
+            type="password"
+            placeholder="confirm password"
+          ></input>
+        </div>
+        <div className="Button-Container">
+          <button>REGISTER</button>
+          <div>
+            <Link to="/login">Already Registered? Login</Link>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
